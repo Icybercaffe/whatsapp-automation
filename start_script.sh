72481ddca7ddbaf4665fe87a02dc4ea60f945f76
Icybercaffe <trendyplane@gmail.com>
2025-06-02 21:18:32 +0200

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 30
-source /home/trendy/whatsapp_automation/venv/bin/activate
-python3 /home/trendy/whatsapp_automation/send_whatsapp_selenium.py >> /home/trendy/whatsapp_automation/autostart.log 2>&1
+source "$HOME/whatsapp_automation/venv/bin/activate"
+python3 "$HOME/whatsapp_automation/send_whatsapp_selenium.py" >> "$HOME/whatsapp_automation/autostart.log" 2>&1
+
